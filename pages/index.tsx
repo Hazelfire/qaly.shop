b3cedd3cecee068684a75558a2b52773a49aa5d6
Sam Nolan <samnolan555@gmail.com>
2023-05-22 13:59:16 +1000

    Half complete next phase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,41 @@
 import Layout from "./_layout";
+import Image from "next/image";
+import { PrismaClient } from "@prisma/client";
+import { GetStaticProps } from "next";
+import Link from "next/link";
 
-const HomePage = () => {
-  const charities = ["Charity 1", "Charity 2", "Charity 3", "Charity 4"];
+const prisma = new PrismaClient();
+type Charity = {
+  id: number;
+  name: string;
+  logoUrl: string;
+  description: string;
+};
 
+type HomePageProps = {
+  charities: [];
+};
+
+const HomePage = ({ charities }: HomePageProps) => {
   return (
     <Layout>
-      <h1 className="title">Charities</h1>
+      <h1 className="title">Welcome to QALY.shop</h1>
+      <p>
+        At QALY.shop, we allow you to read up on the latest research and
+        cost-effectiveness estimates and buy QALYs either directly or through
+        gift cards.
+      </p>
+      <h2 className="subtitle">Charities</h2>
       <ul className="charity-list">
-        {charities.map((charity, index) => (
-          <li key={index} className="charity-item">
-            {charity}
+        {charities.map((charity: Charity) => (
+          <li key={charity.id} className="charity-item">
+            <Link href={`/charity/${charity.id}`}>
+              <a>
+                <Image src={charity.logoUrl} alt={`${charity.name} logo`} />
+                <h3>{charity.name}</h3>
+                <p>{charity.description}</p>
+              </a>
+            </Link>
           </li>
         ))}
       </ul>
@@ -18,6 +44,11 @@ const HomePage = () => {
         .title {
           text-align: center;
           margin-top: 20px;
+        }
+
+        .subtitle {
+          text-align: center;
+          margin-top: 10px;
         }
 
         .charity-list {
@@ -34,9 +65,23 @@ const HomePage = () => {
           border: 1px solid #ccc;
           border-radius: 5px;
         }
+
+        img {
+          height: 100px;
+          width: 100px;
+          object-fit: cover;
+        }
       `}</style>
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const charities = await prisma.charity.findMany({
+    select: { id: true, name: true, logoUrl: true, description: true },
+  });
+
+  return { props: { charities } };
 };
 
 export default HomePage;
--- shell.nix
+++ shell.nix
@@ -3,6 +3,7 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     sqlite
+    docker-compose
     postgresql
   ];
 }
